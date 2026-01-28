cdfacec6ae1cac66e2f5dc870fa56c14f0d27071
Hanh-HE180572 <nghoanganh218@gmail.com>
2026-01-28 16:18:16 +0700

    step3

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,10 +1,29 @@
 ﻿namespace ConsoleApp1
 {
-    internal class Program
+    using System;
+
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Nhấn phím 1 để Chào, phím 2 để Tạm biệt:");
+
+            while (true)
+            {
+                var key = Console.ReadKey(true).Key;
+
+                if (key == ConsoleKey.D1)
+                {
+                    // Step 3
+                    Console.WriteLine("xin chào");
+                }
+                else if (key == ConsoleKey.D2)
+                {
+                    // Step 6
+                    Console.WriteLine("tạm biệt");
+                }
+            }
         }
     }
 }
+
