04dd8971cdbcc8558db4559d9039972b1de3539b
hoangson-1234 <danghoangson2752k4@gmail.com>
2026-01-29 08:46:39 +0700

    Update Step 3+6

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
+            //step 3
             Console.WriteLine("Hello");
+            //step 6
             Console.WriteLine("Bye");
         }
     }
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91e658be75376ad2f5e0c0db67e98b81329b2f67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+682d6510a263c457839f7d782b99452cdac3e562")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
